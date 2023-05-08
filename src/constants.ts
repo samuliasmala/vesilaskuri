e7248d10f7291262d6901d70f6acae089ec71187
Samuli Asmala <samuli.asmala@gmail.com>
2023-05-08 20:47:23 +0300

    Update account number

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const WATER_EUR_PER_M3 = 1.65;
 export const YEARLY_FEE = 40;
 export const INVOICE_RECEIVER = 'Pinsiön alueen vesiosuuskunta';
-export const ACCOUNT_NUMBER = 'FI86 4436 3323 3323 33';
+export const ACCOUNT_NUMBER = 'FI50 1158 3000 2082 41';
 export const DUE_DATE = new Date(2023, 5 - 1, 31);
