9ae7f4f43bba1625b03e58c5824d221300a2a122
Samuli Asmala <samuli.asmala@gmail.com>
2023-05-09 18:59:30 +0300

    Don't use window.location to prevent SSR error

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -2,8 +2,6 @@ import 'client-only';
 
 export function getUrl(relativeUrl: string, params?: Record<string, string>) {
   // Create url with query params
-  const url = new URL(relativeUrl, window.location.origin);
-  if (params) url.search = new URLSearchParams(params).toString();
-
-  return url;
+  if (!params) return relativeUrl;
+  return `${relativeUrl}?${new URLSearchParams(params).toString()}`;
 }
