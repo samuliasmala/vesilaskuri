f4d44679437d1b6877f86a301331899f7f2c450d
Samuli Asmala <samuli.asmala@gmail.com>
2024-04-30 16:17:18 +0300

    Update yearly fee and due date for 2024

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const WATER_EUR_PER_M3 = 1.65;
-export const YEARLY_FEE = 40;
+export const YEARLY_FEE = 30;
 export const INVOICE_RECEIVER = 'Pinsiön alueen vesiosuuskunta';
 export const ACCOUNT_NUMBER = 'FI50 1158 3000 2082 41';
-export const DUE_DATE = new Date(2023, 5 - 1, 31);
+export const DUE_DATE = new Date(2024, 5 - 1, 31);
