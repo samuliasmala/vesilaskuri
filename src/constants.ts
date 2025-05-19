0a4301da98de6ff2236fa1976a1df90087be7e69
Samuli Asmala <samuli.asmala@gmail.com>
2025-05-19 22:40:00 +0300

    Update water price and due date for 2025

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const WATER_EUR_PER_M3 = 1.65;
+export const WATER_EUR_PER_M3 = 1.85;
 export const YEARLY_FEE = 30;
 export const INVOICE_RECEIVER = 'Pinsiön alueen vesiosuuskunta';
 export const ACCOUNT_NUMBER = 'FI50 1158 3000 2082 41';
-export const DUE_DATE = new Date(2024, 5 - 1, 31);
+export const DUE_DATE = new Date(2025, 5 - 1, 31);
 export const VAT = 25.5;
