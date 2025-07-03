4d9cc5315e69d45f6e4490779562494358c63bba
Samuli Asmala <samuli.asmala@gmail.com>
2025-07-03 09:08:27 +0300

    Add invoice barcode

--- src/app/lasku/InvoiceDetails.tsx
+++ src/app/lasku/InvoiceDetails.tsx
@@ -2,6 +2,7 @@
 
 import React, { FC, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
+import Barcode from 'react-barcode';
 import { Button } from '~/components/Button';
 import { Modal } from '~/components/Modal';
 import { H2 } from '~/components/Typography';
@@ -15,7 +16,7 @@ export const InvoiceDetails: FC<{ totalPrice: number; reference: string }> = ({
 }) => {
   const [showBarcodeModal, setShowBarcodeModal] = useState(false);
 
-  let barcode: string;
+  let barcode: string | undefined;
   try {
     barcode = calculateVirtualBarcode({
       account: ACCOUNT_NUMBER,
@@ -26,7 +27,6 @@ export const InvoiceDetails: FC<{ totalPrice: number; reference: string }> = ({
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
-    barcode = 'Viivakoodia ei voitu laskea';
   }
 
   return (
@@ -54,13 +54,35 @@ export const InvoiceDetails: FC<{ totalPrice: number; reference: string }> = ({
         <div>Viite:</div>
         <div>{reference}</div>
         <div>Virtuaali­viivakoodi:</div>
-        <div className="break-all">{barcode}</div>
+        <div className="break-all">
+          {barcode ?? 'Viivakoodia ei voitu laskea'}
+        </div>
+        <div className="col-span-2">
+          {
+            /* Reference is mandatory for the barcode even though it's optional
+               for virtual barcode */
+            barcode !== undefined && reference !== '' && (
+              <div className="mt-2 flex justify-center">
+                <Barcode
+                  format="CODE128C"
+                  value={barcode}
+                  width={1.5}
+                  height={50}
+                  fontSize={12}
+                  margin={5}
+                  displayValue={false}
+                />
+              </div>
+            )
+          }
+        </div>
       </div>
       <div className="mt-10 flex justify-center print:hidden">
         <Button
           className="w-full max-w-xs p-5"
+          disabled={barcode === undefined}
           onClick={() => {
-            void navigator?.clipboard?.writeText(barcode);
+            void navigator?.clipboard?.writeText(barcode ?? '');
             setShowBarcodeModal(true);
           }}
         >
